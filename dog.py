7b0d4d2b2e7618666d3808a089283b650f6a838e
John Montejano <johnmontejano@Johns-MacBook-Pro.local>
2019-09-22 14:46:17 -0700

    append abilities

--- dog.py
+++ dog.py
@@ -10,25 +10,32 @@ class Dog:
     def roll_over(self):
         print('roll!')
 class Ability:
-    def __init__(self, name, max_damage):
+    def __init__(self, name, attack_strength):
         self.name = name 
-        self.max_damage = max_damage
+        self.attack_strength = attack_strength
     def attack(self):
-      return random.randint(0, self.max_damage)
+        return random.randint(0, self.attack_strength)
 
       
 
 class Armor:
-    def __init__(self, name, starting_health):
+    def __init__(self, name, max_block):
         self.name = name
-        self.health = starting_health
-
+        self.max_block = max_block
+    def block(self):
+        return random.randint(0, self.max_block)
+        
+  
 class Hero:
-    def __init__(self, name, starting_health = 100):
+    def __init__(self, name, current_health, starting_health = 100):
         self.name = name 
         self.starting_health = starting_health
-    def add_ability(self, Ability):
-        pass
+        self.current_health = current_health
+        self.armors = list()
+        self.abilities = list()
+    def add_ability(self, ability):
+        self.abilities.append(ability)
+        
     def attack(self):
         pass
     def defend(self, incoming_damage):
@@ -41,6 +48,17 @@ class Hero:
         pass 
 
 if __name__ == "__main__":
-    ability = Ability('debbuging ability', 20)
+    #ABILITY TEST
+    ability = Ability('Dubbger', 50)
+
+    my_hero = Hero('John', 200)
+    my_hero.add_ability(ability)
+    print(my_hero.abilities)
+
+    # !!! I  NEED HELP IN MY CURRENT_HEALTH VARIABLE
+    print(my_hero.current_health)
+    
+    ability = Ability('debbuger name', 20)
     print(ability.name)
-    print(ability.attack())
+    
+
